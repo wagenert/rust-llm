07635bd5ecbe5da68f5289fd249ba7b4325085a6
Thomas Wagener <thomas.wagener@gls-group.com>
2026-07-30 09:56:23 +0200

    Added casual attention algorithm

--- casual-attention/src/main.rs
+++ casual-attention/src/main.rs
@@ -1,4 +1,5 @@
-use burn::{backend::Flex, tensor::backend::BackendTypes};
+use burn::{Tensor, backend::Flex, tensor::{backend::BackendTypes, Float}};
+use casual_attention::casual_attention::CasualAttention;
 
 type MyBackend = Flex<f32, i32>;
 
@@ -13,8 +14,12 @@ fn main() {
     ];
 
     let device = <Flex::<f32, i32> as BackendTypes>::Device::default();
+    let input_tensor = Tensor::<MyBackend, 2, Float>::from_data(inputs, &device);
+    let d_in = input_tensor.shape()[1];
+    let d_out = 2;
 
-    let sa_v1 = SelfAttention::<MyBackend>::new(d_in, d_out, false, &device);
+
+    let sa_v1 = CasualAttention::<MyBackend>::new(d_in, d_out, 0.5, false, &device);
     let outputs = sa_v1.forward(input_tensor.clone());
     println!("Outputs: {}", outputs);
 }
