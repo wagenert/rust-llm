e29523282fed3a2c04293aee3cfb545aaae788a1
Thomas Wagener <thomas.wagener@fischantiquariat.de>
2026-08-05 08:23:50 +0200

    Find solution for broadcastable masked fill

--- ch03/multi-head-attention/src/multi_head_attention.rs
+++ ch03/multi-head-attention/src/multi_head_attention.rs
@@ -1,4 +1,5 @@
 use burn::nn::{Dropout, DropoutConfig, Linear, LinearConfig};
+use burn::tensor::{Bool, Int};
 use burn::tensor::activation::softmax;
 use burn_helpers::casual_attention::CasualAttention;
 use burn::tensor::{backend::Backend, Tensor};
@@ -37,10 +38,11 @@ pub struct MultiHeadAttention<B: Backend> {
     w_value: Linear<B>,
     out_proj: Linear<B>,
     dropout: Dropout,
+    mask: Tensor<B, 2, Bool>,
 }
 
 impl<B: Backend> MultiHeadAttention<B> {
-    pub fn new(d_in: usize, d_out: usize, dropout: f64, num_heads: usize, qkv_bias: bool, device: B::Device) -> Self {
+    pub fn new(d_in: usize, d_out: usize, context_length: usize, dropout: f64, num_heads: usize, qkv_bias: bool, device: B::Device) -> Self {
         assert!(d_out % num_heads == 0, "d_out must be divisible by num_heads");
         let head_dim = d_out / num_heads;
         let w_query = LinearConfig::new(d_in, d_out).with_bias(qkv_bias).init(&device);
@@ -48,6 +50,7 @@ impl<B: Backend> MultiHeadAttention<B> {
         let w_value = LinearConfig::new(d_in, d_out).with_bias(qkv_bias).init(&device);
         let out_proj = LinearConfig::new(d_out, d_out).with_bias(qkv_bias).init(&device);
         let dropout = DropoutConfig::new(dropout).init();
+        let mask = Tensor::triu_mask([context_length, context_length], 1, &device);
         Self {
             d_out,
             num_heads,
@@ -57,6 +60,7 @@ impl<B: Backend> MultiHeadAttention<B> {
             w_value,
             out_proj,
             dropout,
+            mask,
         }
     }
 
@@ -64,23 +68,30 @@ impl<B: Backend> MultiHeadAttention<B> {
         let shape = input.shape();
         let batch_size = shape[0];
         let num_tokens = shape[1];
-        let d_in = shape[2];
-        let keys = self.w_key.forward(input.clone()).reshape([batch_size, num_tokens, self.num_heads, self.head_dim]);
-        let queries = self.w_query.forward(input.clone()).reshape([batch_size, num_tokens, self.num_heads, self.head_dim]);
-        let values = self.w_value.forward(input).reshape([batch_size, num_tokens, self.num_heads, self.head_dim]);
-        let keys = keys.permute([0, 2, 1, 3]);
-        let queries = queries.permute([0, 2, 1, 3]);
-        let values = values.permute([0, 2, 1, 3]);
-        // Perform scaled dot-product attention
-        let attn_scores = queries.matmul(keys.permute([0, 1, 3, 2]));
-        let score_mask = Tensor::tril_mask(attn_scores.shape(), 0, &attn_scores.device());
-        let masked_scores = Tensor::mask_fill(attn_scores, score_mask, f32::NEG_INFINITY);
-        let softmax_dim = masked_scores.dims().len() - 1;
-        let attn_weights = softmax(masked_scores / (self.head_dim as f32).sqrt(), softmax_dim);
+        let _d_in = shape[2];
+        let keys = self.w_key.forward(input.clone())
+            .reshape([batch_size, num_tokens, self.num_heads, self.head_dim])
+            .swap_dims(1, 2);
+        let queries = self.w_query.forward(input.clone())
+            .reshape([batch_size, num_tokens, self.num_heads, self.head_dim])
+            .swap_dims(1, 2);
+        let values = self.w_value.forward(input)
+            .reshape([batch_size, num_tokens, self.num_heads, self.head_dim])
+            .swap_dims(1, 2);
+
+            // Perform scaled dot-product attention
+        let attn_scores = queries.matmul(keys.clone().swap_dims(2, 3));
+        //let score_mask = Tensor::tril_mask(attn_scores.shape(), 0, &attn_scores.device());
+        let attn_scores = Tensor::mask_fill(attn_scores, self.mask.clone(), f32::NEG_INFINITY);
+        let softmax_dim = attn_scores.dims().len() - 1;
+        let key_dim = *keys.shape().last().unwrap();
+        let attn_weights = softmax(attn_scores / (key_dim as f32).sqrt(), softmax_dim);
         let attn_weights = self.dropout.forward(attn_weights);
 
-        let context_vec = attn_weights.matmul(values);
-        let context_vec = context_vec.permute([0, 2, 1, 3]).reshape([batch_size, num_tokens, self.d_out]);
+        let context_vec = attn_weights.matmul(values)
+            .swap_dims(1, 2)
+            .reshape([batch_size, num_tokens, self.d_out]);
+        // let context_vec = context_vec.permute([0, 2, 1, 3]).reshape([batch_size, num_tokens, self.d_out]);
         let context_vec = self.out_proj.forward(context_vec);
         context_vec
     }
