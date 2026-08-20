47e9bfac3b41d2db0ea3e33eece9bb19f6e51404
Thomas Wagener <thomas.wagener@gls-group.com>
2026-08-20 11:39:06 +0200

    Added main function

--- ch04/full_model/src/main.rs
+++ ch04/full_model/src/main.rs
@@ -16,6 +16,19 @@ fn main() {
     };
     let device = <ComputeBackend as burn::tensor::backend::BackendTypes>::Device::default();
     ComputeBackend::seed(&device, 123);
+    let txt1 = "Every effort moves you";
+    let txt2 = "Every day holds a";
+
+    let tokenizer = tiktoken::get_encoding("gpt2").unwrap();
+    let enc_txt1: Tensor<ComputeBackend, 1, Int> =
+        Tensor::from_data(tokenizer.encode(txt1).as_slice(), &device.clone());
+    let enc_txt2: Tensor<ComputeBackend, 1, Int> =
+        Tensor::from_data(tokenizer.encode(txt2).as_slice(), &device.clone());
+    let batch: Tensor<ComputeBackend, 2, Int> = Tensor::stack([enc_txt1, enc_txt2].to_vec(), 0);
 
     let model = GptModel::<ComputeBackend>::new(&config, device.clone());
+    let output = model.forward(batch.clone());
+    println!("Input shape: {:?}", batch.shape());
+    println!("Output shape: {:?}", output.shape());
+    println!("Output: {:?}", output);
 }
